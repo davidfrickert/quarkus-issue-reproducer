b5b0622aa038290b2f9fbbcd940c67211be218bf
David Frickert <david.frickert@protonmail.com>
2023-01-18 20:15:17 +0000

    Reproducer for quarkus #30445

--- src/main/java/org/acme/GreetingResourceImpl.java
+++ src/main/java/org/acme/GreetingResourceImpl.java
@@ -1,8 +1,30 @@
 package org.acme;
 
-public class GreetingResourceImpl implements GreetingResource {
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
+import javax.annotation.security.RolesAllowed;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@RequestScoped
+@Path("/hello")
+public class GreetingResourceImpl {
+
+    private final AuthenticationIdentity authenticationIdentity;
+
+    @Inject
+    public GreetingResourceImpl(AuthenticationIdentity authenticationIdentity) {
+        this.authenticationIdentity = authenticationIdentity;
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @RolesAllowed({"arole"})
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello " + authenticationIdentity.getSubject();
     }
 }
--- src/test/java/org/acme/GreetingResourceImplTest.java
+++ src/test/java/org/acme/GreetingResourceImplTest.java
@@ -1,6 +1,9 @@
 package org.acme;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.security.TestSecurity;
+import io.quarkus.test.security.jwt.Claim;
+import io.quarkus.test.security.jwt.JwtSecurity;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,12 +13,23 @@ import static org.hamcrest.CoreMatchers.is;
 public class GreetingResourceImplTest {
 
     @Test
+    @TestSecurity(user = "testUser", roles = {"arole"})
+    @JwtSecurity(
+            claims = {
+                    @Claim(key = "language", value = "pt"),
+                    @Claim(key = "country", value = "PT"),
+                    @Claim(key = "exp", value = "9999999999"),
+                    @Claim(key = "iat", value = "0"),
+                    @Claim(key = "azp", value = "test"),
+                    @Claim(key = "sub", value = "testUser"),
+                    @Claim(key = "realm_access", value = "{\"roles\":[\"arole\"]}")
+    })
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(401)
-             .body(is("Hello from RESTEasy Reactive"));
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("Hello testUser"));
     }
 
 }
